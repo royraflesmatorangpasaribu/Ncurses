4e3a0980b95ef45f9eaf373cab232828d269e819
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-18 21:37:08 +0700

    Update sad.cpp

--- sad.cpp
+++ sad.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main(){
 	//memulai layar ncurses
 	initscr();
-
+	
+	//menampilkan output dengan baris dan kolom
 	mvprintw (10,43,"FROM HAHAHA TO YA GITU LAH :'(");
 
 	refresh();
 	getch();
 	
+	//menghapus layar ncurses
 	endwin();
 }
