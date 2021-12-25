9708d9adf0a8c71a253a4cfaeaf5ecbb08c6f68e
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-25 22:19:31 +0700

    Add files via upload

---
+++ natalprogram.cpp
@@ -0,0 +1,36 @@
+#include<ncurses/ncurses.h>
+#include<windows.h>
+
+int main(){
+	char nama[100];
+
+	//memulai layar ncurses
+	initscr();
+	
+	//mencetak output di baris... dan kolom...
+	mvprintw(10, 40, "Masukkan Nama Anda    : ");
+	//input
+	getstr(nama);
+	
+	//looping loading
+	for(int baris=48; baris<=59; baris++){
+		mvprintw(11, baris, "o");
+		refresh();
+		Sleep(100);
+	}
+	clear();
+	while(true){
+		//membersihkan layar ncurses
+		clear();
+		
+		//mencetak output di baris... dan kolom...
+		mvprintw(10, 48, "Selamat Natal 2021");
+		mvprintw(11, 48, " ");
+		printw(nama);
+		refresh();
+		Sleep(500);
+			
+	}
+	getch();
+	endwin();
+}
