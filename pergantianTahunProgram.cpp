0a03a61207a6ca36c1ef5c5181c23ea17710d8e9
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-31 11:25:39 +0700

    Add files via upload

---
+++ pergantianTahunProgram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+//fungsi prosedur waktu
+void waktu(){
+	
+	//deklarasi variabel
+	int i;
+	cout<<"Running Program ini 1 menit sebelum Pergantian Tahun"<<endl;
+	for(int i=3; i>=0 ; i--){
+		cout << "waktu : ";
+		cout << i;
+		Sleep(2000);   //jeda 2 detik
+		system("cls");	//menghapus layar
+	}
+}
+//minimal terdapat 1 fungsi 
+int main(){
+	//memanggil prosedur fungsi waktu
+	waktu();
+	cout<<"Selamat Tahun Baru 2022 Semoga Tahun ini menjadi tahun yang lebih baik dari tahun 2021"<<endl;
+	cout<<"\t\t\t\t\tTuhan Memberkati";
+}
